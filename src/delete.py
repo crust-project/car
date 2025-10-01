3030578b5b4a8d4b39a631e20a1837f7170e0824
Juraj Kollár <mostypc7@gmail.com>
2025-10-01 19:07:38 +0200

    Update delete.py

--- src/delete.py
+++ src/delete.py
@@ -4,15 +4,37 @@ from rich.console import Console
 
 console = Console()
 
+def status(msg, level="info"):
+    colors = {
+        "info": "blue",
+        "ok": "green",
+        "error": "red",
+    }
+    color = colors.get(level, "blue") + " bold"
+    console.print("::", style=color, end=" ")
+    console.print(msg)
+
 def main(pkgname):
-    console.log(f"[cyan]Uninstalling [bold]{pkgname}[/bold]...")
+    if not os.path.isfile("/home/" + os.getlogin() + "/.config/repro.car"):
+        status("No repro.car file. Creating", "warn")
+        with open("/home/" + os.getlogin() + "/.config/repro.car", "w") as f:
+            f.write()
+    else:
+        with open("/home/" + os.getlogin() + "/.config/repro.car", "r") as f:
+            pkgs_installed = f.read()
+            pkgs_installed = pkgs_installed.replace(pkgname, "")
+        with open("/home/" + os.getlogin() + "/.config/repro.car", "w") as f:
+            f.write(pkgs_installed)
+
+    status(f"Uninstalling {pkgname}...", "info")
     try:
-        exit_code = os.system(f"sudo rm -f /usr/bin/{pkgname}")
+        exit_code = os.system(f"sudo rm /usr/bin/{pkgname}")
         if exit_code != 0:
-            console.log(f"[red]Failed to uninstall [bold]{pkgname}[/bold]")
+            status(f"Failed to uninstall {pkgname}", "error")
             sys.exit(1)
-        console.log(f"[green]Successfully uninstalled [bold]{pkgname}[/bold]")
+        status(f"Successfully uninstalled {pkgname}", "ok")
     except Exception:
+        console.print("::", style="red", end=" ")
+        console.print("Unhandled exception")
         console.print_exception()
         sys.exit(1)
-
