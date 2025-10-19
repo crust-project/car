5ae7b25395b13e76f0dededf3b12b5e2a5a2b255
Juraj Kollár <mostypc7@gmail.com>
2025-10-19 20:53:58 +0200

    Add search command to package manager

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import argparse
 import install
 import delete
 import update
+import search
 
 parser = argparse.ArgumentParser(description="A simple package manager")
 subparsers = parser.add_subparsers(dest="command", required=True)
@@ -18,6 +19,10 @@ p_delete.add_argument("package", help="Package to delete")
 p_update = subparsers.add_parser("update", help="Update a package")
 p_update.add_argument("package", help="Package to update")
 
+# search
+p_search = subparsers.add_parser("search", help="Search for a package")
+p_search.add_argument("package", help="Package to search for") 
+
 args = parser.parse_args()
 
 if args.command == "get":
@@ -26,4 +31,6 @@ elif args.command == "delete":
     delete.main(args.package)
 elif args.command == "update":
     update.main(args.package)
+elif args.command == "search":
+    search.main(args.package)
 
